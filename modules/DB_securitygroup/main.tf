b3fced3192789bcf5356277ce1e6da8cb46e2967
Daicon001 <115258966+Daicon001@users.noreply.github.com>
2022-10-10 11:04:09 +0100

    Update main.tf

--- modules/DB_securitygroup/main.tf
+++ modules/DB_securitygroup/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "Database_sg" {
   }
 }
 
-resource "aws_security_group_rule" "allow_http" {
+resource "aws_security_group_rule" "allow_mysql" {
   type="ingress"
   security_group_id = aws_security_group.Database_sg.id
   from_port = var.mysql
